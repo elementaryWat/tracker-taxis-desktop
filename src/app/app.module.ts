e4edd105a67d39d24ef88e48852adff684fa65ba
Augusto Romero <geragusto@hotmail.com>
2018-06-14 11:03:57 -0300

    Integration With Maps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
 
 
 @NgModule({
@@ -10,7 +11,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCnZ337BZVTzWsTcpcIAUkgqmVoaq4X69I'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
